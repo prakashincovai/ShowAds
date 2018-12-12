c30e33985dd0ea1ec8d3f3a1e59d1b9a53be2a1c
prakashincovai <prakashincovai@gmail.com>
2018-12-12 18:04:14 +0530

    Update ads-rotator.js

--- Basic Ads/js/ads-rotator.js
+++ Basic Ads/js/ads-rotator.js
@@ -1,18 +1,20 @@
 $(function(){
   var adBanners = [
-       "google.png",
+       "GoDaddy728x90",
        "flipkart728x90.jpg",
-       "samsung728x90.jpg"
+       "samsung728x90.jpg",
+       "GoDaddy728x901"
   ];
   var bannerLinks = [
-       "http://google.com",
+       "https://clnk.in/gXeG",
        "http://www.flipkart.com/offers/electronics?affid=prakashincovai",
-       "https://goo.gl/y4tS3k"
+       "https://goo.gl/y4tS3k",
+       "https://clnk.in/gXeG"
   ];
   
   
   var imgPrefix   = "https://raw.githack.com/prakashincovai/ShowAds/master/Basic Ads/images/";
-  var randNum     = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+  var randNum     = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
   var topAdBanner = $('#topad > a > img');
   
   var newBannerImg  = imgPrefix + adBanners[randNum];
