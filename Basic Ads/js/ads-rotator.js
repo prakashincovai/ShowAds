ef7af6a828ff0c6080dcbeb7ca8f873e64bbd6e3
prakashincovai <prakashincovai@gmail.com>
2018-12-14 17:37:21 +0530

    Update ads-rotator.js

--- Basic Ads/js/ads-rotator.js
+++ Basic Ads/js/ads-rotator.js
@@ -4,13 +4,13 @@ if (Cus_analyticsId!=='') {
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        ga('create',Cus_analyticsId, 'auto', 'blogger');
-        ga('blogger.send', 'pageview');
+        gtag1('create',Cus_analyticsId, 'auto', 'blogger');
+        gtag1('blogger.send', 'pageview');
 
 window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config',Cus_analyticsId);
+  gtag1('js', new Date());
+  gtag1('config',Cus_analyticsId);
 };
 // Overall analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
