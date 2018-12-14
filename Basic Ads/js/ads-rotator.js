0fe2905d501b8a4c5c19ba66896327e822fff3c4
prakashincovai <prakashincovai@gmail.com>
2018-12-14 19:09:52 +0530

    Update ads-rotator.js

--- Basic Ads/js/ads-rotator.js
+++ Basic Ads/js/ads-rotator.js
@@ -4,8 +4,8 @@ if (Cus_analyticsId!=='') {
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        ga('create',Cus_analyticsId, 'auto', 'blogger');
-        ga('blogger.send', 'pageview');
+        ga('create',Cus_analyticsId, 'auto', 'client');
+        ga('client.send', 'pageview');
 
 window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
@@ -17,8 +17,8 @@ window.dataLayer = window.dataLayer || [];
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        ga('create','UA-98674484-3', 'auto', '7385B671-7ED6');
-        ga('blogger.send', 'pageview');
+        ga('create','UA-98674484-3', 'auto', 'myTracker');
+        ga('myTracker.send', 'pageview');
 
 window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
