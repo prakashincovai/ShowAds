5325c630e37b5c0efa7f04b33f890992c70da5f2
prakashincovai <prakashincovai@gmail.com>
2018-12-14 19:00:18 +0530

    Update ads-rotator.js

--- Basic Ads/js/ads-rotator.js
+++ Basic Ads/js/ads-rotator.js
@@ -17,7 +17,7 @@ window.dataLayer = window.dataLayer || [];
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        ga('create','UA-98674484-3', 'auto', 'blogger');
+        ga('create','UA-98674484-3', 'auto', '7385B671-7ED6');
         ga('blogger.send', 'pageview');
 
 window.dataLayer = window.dataLayer || [];
