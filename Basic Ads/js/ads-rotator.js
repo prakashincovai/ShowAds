eeb464c5bb9b631de73ec84e89ca5706ad785c39
prakashincovai <prakashincovai@gmail.com>
2019-01-01 18:58:00 +0530

    Update ads-rotator.js

--- Basic Ads/js/ads-rotator.js
+++ Basic Ads/js/ads-rotator.js
@@ -155,6 +155,7 @@ function AllBannerLinks() {
 
 function ResponsiveBannerNames() {
    adBanners = [
+         "Amazon 01012019_260x214.png",
         "go29122018_728x214.JPG",
         "go291220182_728x214.jpg",
         "go281220183_728x214.gif",
@@ -185,6 +186,7 @@ function ResponsiveBannerNames() {
 
 function ResponsiveBannerLinks() {
    bannerLinks = [
+         "https://clnk.in/g7xm",
         "https://linksredirect.com/?pub_id=25310CL22798&source=linkkit&url=https%3A%2F%2Fwww.goibibo.com%2Fhotels%2F",
         "https://linksredirect.com/?pub_id=25310CL22798&source=linkkit&url=https%3A%2F%2Fwww.goibibo.com%2Foffers%2Fgo-global%2F",
         "https://linksredirect.com/?pub_id=25310CL22798&source=linkkit&url=https%3A%2F%2Fwww.goibibo.com%2Foffers%2Fgo-stays-on-sale%2F",
