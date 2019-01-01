228953ffea431350994831404f8cc578cdac8b89
prakashincovai <prakashincovai@gmail.com>
2019-01-01 13:57:37 +0530

    Update ads-rotator.js

--- Basic Ads/js/ads-rotator.js
+++ Basic Ads/js/ads-rotator.js
@@ -105,7 +105,11 @@ if (adBanners.length!=bannerLinks.length)
   $(topAdBanner).attr('src',newBannerImg);
   $('#topad > a').attr('href',newBannerLink);
         
-document.getElementById("UAds").innerHTML = "<link type=\"text/css\" rel=\"stylesheet\" href=\"https://rawcdn.githack.com/prakashincovai/ShowAds/c5f5323b51acd028a3e09fe1aa39d0f3c65227ae/Basic%20Ads/css/Res.css\"> \n <div id=\"w\">\n <div id=\"topad\">\n <a href="+newBannerLink+"><img src="+newBannerImg+ " alt=\"\" class=\"res\"/></a>\n </div> </div>\n";
+document.getElementById("UAds").innerHTML = "<link type=\"text/css\" rel=\"stylesheet\" \
+' href=\"https://rawcdn.githack.com/prakashincovai/ShowAds/c5f5323b51acd028a3e09fe1aa39d0f3c65227ae/Basic%20Ads/css/Res.css\"> \
+<div id=\"w\">\n <div id=\"topad\">\n <a href="+newBannerLink+">\n \ 
+<img src="+newBannerImg+ " alt=\"\" class=\"res\"/></a>\n </div> </div>\n";
+        
 })
 // Main Ads End
 
