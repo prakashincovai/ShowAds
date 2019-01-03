05a9b84db9177de0d3bb8549d0cbc949726404cd
prakashincovai <prakashincovai@gmail.com>
2019-01-03 18:23:18 +0530

    Update ads-rotator.js

--- Basic Ads/js/ads-rotator.js
+++ Basic Ads/js/ads-rotator.js
@@ -96,14 +96,14 @@ if (adBanners.length!=bannerLinks.length)
    
   var imgPrefix   = "https://raw.githack.com/prakashincovai/ShowAds/master/Basic%20Ads/images/";
   var randNum     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
-  var topAdBanner = $('#topad > a > img');
+  // var topAdBanner = $('#topad > a > img');
   
   var newBannerImg  = imgPrefix + adBanners[randNum];
   var newBannerLink = bannerLinks[randNum];
   
   // update new img src and link HREF value
-  $(topAdBanner).attr('src',newBannerImg);
-  $('#topad > a').attr('href',newBannerLink);
+ // $(topAdBanner).attr('src',newBannerImg);
+ // $('#topad > a').attr('href',newBannerLink);
         
 document.getElementById("UAds").innerHTML = "<link type=\"text/css\" rel=\"stylesheet\" \
 ' href=\"https://rawcdn.githack.com/prakashincovai/ShowAds/c5f5323b51acd028a3e09fe1aa39d0f3c65227ae/Basic%20Ads/css/Res.css\"> \
