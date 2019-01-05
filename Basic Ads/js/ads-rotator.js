d349d65486579fa9ce0c27dce3ad6c74cc1c9dbf
prakashincovai <prakashincovai@gmail.com>
2019-01-05 14:15:10 +0530

    Update ads-rotator.js

--- Basic Ads/js/ads-rotator.js
+++ Basic Ads/js/ads-rotator.js
@@ -287,6 +287,9 @@ function ResponsiveBannerLinks() {
 // Size 300x300 Responsive
 function BannerNames300x300() {
    adBanners = [
+       "Lim05012019_300x300.jpeg",
+       "Pay05012019_300x300.jpg",
+       "ja05012019_300x300.jpg",
        "f04012019_300x300.jpeg",
        "f104012019_300x300.jpeg",
        "f304012019_300x300.jpeg"
@@ -295,6 +298,9 @@ function BannerNames300x300() {
 
 function BannerLinks300x300() {
    bannerLinks = [
+       "https://clnk.in/g9sQ",
+       "https://clnk.in/g9sR",
+       "https://clnk.in/g9sS",
        "http://fkrt.it/LMbXInuuuN",
        "http://fkrt.it/2HQzDKNNNN",
        "http://fkrt.it/2HWuCKNNNN"
