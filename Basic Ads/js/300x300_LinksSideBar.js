bfdf9f0156e5c16e3c623bc08bd6d30e8c618211
prakashincovai <prakashincovai@gmail.com>
2019-01-07 15:13:58 +0530

    Add files via upload

---
+++ Basic Ads/js/300x300_LinksSideBar.js
@@ -0,0 +1,18 @@
+
+// Size 300x300 Responsive
+function BannerNames300x300() {
+   adBanners = [
+       "f04012019_300x300.jpeg",
+       "f104012019_300x300.jpeg",
+       "f304012019_300x300.jpeg"
+  ];
+}
+
+function BannerLinks300x300() {
+   bannerLinks = [
+       "http://fkrt.it/LMbXInuuuN",
+       "http://fkrt.it/2HQzDKNNNN",
+       "http://fkrt.it/2HWuCKNNNN"
+  ];
+}
+
---
+++ Basic Ads/js/Responsive_LinksCenter.js
@@ -0,0 +1,13 @@
+// Size 728x90 728x214 Responsive
+function ResponsiveBannerNames() {
+   adBanners = [
+       "cars24728x214.jpg"
+  ];
+}
+
+function ResponsiveBannerLinks() {
+   bannerLinks = [
+       "https://clnk.in/g1xR"
+  ];
+}
+
