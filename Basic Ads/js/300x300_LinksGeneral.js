59427b1572975850f7715400a362153d077e2f63
prakashincovai <prakashincovai@gmail.com>
2019-01-07 15:36:43 +0530

    Add files via upload

---
+++ Basic Ads/js/300x300_LinksGeneral.js
@@ -0,0 +1,28 @@
+// Size 300x300 Responsive
+function BannerNames300x300Full() {
+   adBanners = [
+       "cam05012019_300x300.jpeg",
+       "shoo05012019_300x300.jpeg",
+       "cam05012019_300x300.png",
+       "Lim05012019_300x300.jpeg",
+       "Pay05012019_300x300.jpg",
+       "ja05012019_300x300.jpg",
+       "f04012019_300x300.jpeg",
+       "f104012019_300x300.jpeg",
+       "f304012019_300x300.jpeg"
+  ];
+}
+
+function BannerLinks300x300Full() {
+   bannerLinks = [
+       "http://fkrt.it/DqN3znuuuN",
+       "http://fkrt.it/DqEFinuuuN",
+       "http://fkrt.it/DvQMSnuuuN",
+       "https://clnk.in/g9sQ",
+       "https://clnk.in/g9sR",
+       "https://clnk.in/g9sS",
+       "http://fkrt.it/LMbXInuuuN",
+       "http://fkrt.it/2HQzDKNNNN",
+       "http://fkrt.it/2HWuCKNNNN"
+  ];
+}
