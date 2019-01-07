58583c81bd00a50dcb85ab53e6672601862bcd60
prakashincovai <prakashincovai@gmail.com>
2019-01-07 15:42:19 +0530

    Update ads-rotator.js

--- Basic Ads/JS_Controller/ads-rotator.js
+++ Basic Ads/JS_Controller/ads-rotator.js
@@ -40,14 +40,13 @@ window.dataLayer = window.dataLayer || [];
   gtag('config','UA-98674484-3');
 
 // Add Css File
-//  var head = document.getElementsByTagName('head')[0],
-//  link = document.createElement('link');
-//   link.type = 'text/css';
-//  link.rel = 'stylesheet';
-//   link.href = 'css/Res.css';
-//   head.appendChild(link);
+// var head = document.getElementsByTagName('head')[0],
+// link = document.createElement('link');
+// link.type = 'text/css';
+// link.rel = 'stylesheet';
+// link.href = 'https://rawcdn.githack.com/prakashincovai/ShowAds/c5f5323b51acd028a3e09fe1aa39d0f3c65227ae/Basic Ads/css/Res.css';
+// head.appendChild(link);
 
-  
 // Main Ads Start
 $(function(){
         
@@ -60,58 +59,27 @@ if (isNaN(Uid)==true)
 {
 return;
 };
-       
+        
 if (ads_size=='Any') 
 {
   AllBannerNames();
   AllBannerLinks();
 }
-
-//$.getScript('js/Responsive_Links.js', function() {
-  //  //script is loaded and executed put your dependent JS here
-  //ResponsiveBannerNames();
-  //ResponsiveBannerLinks();
-//});
-
-
-
-//Load jQuery
-//var script = document.createElement("SCRIPT");
-//script.setAttribute("type", "text/javascript");
-//script.setAttribute("src", "file://C:/Users/administrator.SIVASAKTHISOFT/Desktop/Show Ads/Basic Ads/js/Responsive_Links.js");
-//document.getElementsByTagName("head")[0].appendChild(script);
-
-
-
 if (ads_size=='Responsive') 
 {
-  	var Rnd =Math.floor(Math.random() * ((2-1) - 0 + 1)) + 0;
-	if (Rnd==0)
-	{
-  	ResponsiveBannerNames();
-  	ResponsiveBannerLinks();
-	}
-	else
-	{
-  	BannerNames300x300();
-  	BannerLinks300x300();
-	}
-
-
-}
-else if(ads_size=='ResponsiveSidebar')
+  var Rnd =Math.floor(Math.random() * ((2-1) - 0 + 1)) + 0;
+if (Rnd==0)
 {
-	var Rnd =Math.floor(Math.random() * ((2-1) - 0 + 1)) + 0;
-	if (Rnd==0)
-	{
-  	ResponsiveBannerNamesSideBar();
-  	ResponsiveBannerLinksSideBar();
-	}
-	else
-	{
-  	BannerNamesSideBar300x300();
-  	BannerLinksSideBar300x300();
-	}
+  ResponsiveBannerNames();
+  ResponsiveBannerLinks();
+}
+else
+{
+  BannerNames300x300();
+  BannerLinks300x300();
+}
+
+
 }
 else
 {
@@ -126,68 +94,78 @@ if (adBanners.length!=bannerLinks.length)
 };
    
 
-
 var strVar="";
-if (ads_size=='Responsive') 
+if (Rnd==0)
 {
-	if (Rnd==0)
-	{
-	  var imgPrefix   = "images/";
-	  var randNum     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
+  var imgPrefix   = "https://raw.githack.com/prakashincovai/ShowAds/master/Basic%20Ads/images/";
+  var randNum     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
+
   
- 	 var newBannerImg  = imgPrefix + adBanners[randNum];
-	  var newBannerLink = bannerLinks[randNum];
+  var newBannerImg  = imgPrefix + adBanners[randNum];
+  var newBannerLink = bannerLinks[randNum];
 
-	 // 728x214 728x90 banners 
-	strVar += "<link type=\"text/css\" rel=\"stylesheet\n \"";
-	strVar += "href=\"https://rawcdn.githack.com/prakashincovai/ShowAds/c5f5323b51acd028a3e09fe1aa39d0f3c65227ae/Basic%20Ads/css/Res.css\">";
-	strVar += "<div id=\"w\">\n <div id=\"topad\">\n <a href="+newBannerLink+">";
-	strVar += "<img src="+newBannerImg+ " alt=\"\" class=\"res\"/></a>\n </div> </div>";
-	}
-	else
-	{ // 300x300 banners
-
-  	var imgPrefix   = "images300x300/";
-	  var randNum     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
-  
-  	var newBannerImg  = imgPrefix + adBanners[randNum];
-	  var newBannerLink = bannerLinks[randNum];
-	//
-	  var imgPrefix2   = "images300x300/";
-	  var randNum2     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
-	  
-	  var newBannerImg2  = imgPrefix2 + adBanners[randNum2];
-	  var newBannerLink2 = bannerLinks[randNum2];
-	
-	  var imgPrefix3   = "images300x300/";
-	  var randNum3     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
-	  
-	  var newBannerImg3  = imgPrefix3 + adBanners[randNum3];
-	  var newBannerLink3 = bannerLinks[randNum3];
-
-
-strVar += "<div style=\"height: 300px;\">";
+ // 728x214 728x90 banners 
 strVar += "<link type=\"text/css\" rel=\"stylesheet\n \"";
-strVar += "href=\"css/Res.css\">";
+strVar += "href=\"https://rawcdn.githack.com/prakashincovai/ShowAds/c5f5323b51acd028a3e09fe1aa39d0f3c65227ae/Basic%20Ads/css/Res.css\">";
+strVar += "<div id=\"w\">\n <div id=\"topad\">\n <a href="+newBannerLink+">";
+strVar += "<img src="+newBannerImg+ " alt=\"\" class=\"res\"/></a>\n </div> </div>";
+}
+else
+{ // 300x300 banners
+
+  var imgPrefix   = "https://raw.githack.com/prakashincovai/ShowAds/master/Basic%20Ads/images300x300/";
+  var randNum     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
+  
+  var newBannerImg  = imgPrefix + adBanners[randNum];
+  var newBannerLink = bannerLinks[randNum];
+//
+  var imgPrefix2   = "https://raw.githack.com/prakashincovai/ShowAds/master/Basic%20Ads/images300x300/";
+  var randNum2     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
+  
+  var newBannerImg2  = imgPrefix2 + adBanners[randNum2];
+  var newBannerLink2 = bannerLinks[randNum2];
+
+  var imgPrefix3   = "https://raw.githack.com/prakashincovai/ShowAds/master/Basic%20Ads/images300x300/";
+  var randNum3     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
+  
+  var newBannerImg3  = imgPrefix3 + adBanners[randNum3];
+  var newBannerLink3 = bannerLinks[randNum3];
+        
+  var imgPrefix4   = "https://raw.githack.com/prakashincovai/ShowAds/master/Basic%20Ads/images300x300/";
+  var randNum4     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
+  
+  var newBannerImg4  = imgPrefix4 + adBanners[randNum4];
+  var newBannerLink4 = bannerLinks[randNum4];
+
+
+strVar += "<div style=\"height: 250px;\">";
+strVar += "<link type=\"text/css\" rel=\"stylesheet\n \"";
+strVar += "href=\"https://rawcdn.githack.com/prakashincovai/ShowAds/c5f5323b51acd028a3e09fe1aa39d0f3c65227ae/Basic%20Ads/css/Res.css\">";
 strVar += "        <table cellpadding=\"0\" border=\"1\" role=\"presentation\">";
 strVar += "            <tr>";
 strVar += "                <td>";
-strVar += "                    <table width=\"100%\" cellspacing=\"0\" cellpadding=\"1\" border=\"1\" align=\"center\">";
+strVar += "                    <table width=\"100%\" cellspacing=\"0\" cellpadding=\"0\" border=\"1\" align=\"center\">";
 strVar += "                        <tbody> ";
 strVar += "                            <tr>";
-strVar += "                                <td width=\"33.33%\">";
+strVar += "                                <td width=\"25%\">";
 strVar += "                                    <a href="+newBannerLink+" style=\"display:block;border:none\" target=\"_blank\" data-saferedirecturl=\"\">";
 strVar += "                                    <img alt=\"xxx\" src="+newBannerImg+ "\n \"";
 strVar += "			            style=\"background-color:#f6f2e9;border:none;color:#818181;display:";
 strVar += "			            block;font-size:9px;max-width:100%\" width=\"100%\"><\/a>";
 strVar += "                                <\/td>";
-strVar += "                                <td width=\"33.33%\">";
+strVar += "                                <td width=\"25%\">";
 strVar += "                                    <a href="+newBannerLink2+" style=\"display:block;border:none\" target=\"_blank\" data-saferedirecturl=\"\">";
 strVar += "                                    <img alt=\"xxx\" src="+newBannerImg2+ " \n \"";
 strVar += "			            style=\"background-color:#f6f2e9;border:none;color:#818181;display:";
 strVar += "			            block;font-size:9px;max-width:100%\" width=\"100%\"><\/a>";
 strVar += "                                <\/td>";
-strVar += "                                <td width=\"33.33%\">";
+strVar += "                                <td width=\"25%\">";
+strVar += "                                    <a href="+newBannerLink3+" style=\"display:block;border:none\" target=\"_blank\" data-saferedirecturl=\"\">";
+strVar += "                                    <img alt=\"xxx\" src="+newBannerImg3+ " \n \"";
+strVar += "			            style=\"background-color:#f6f2e9;border:none;color:#818181;display:";
+strVar += "			            block;font-size:9px;max-width:100%\" width=\"100%\"><\/a>";
+strVar += "                                <\/td>";
+strVar += "                                <td width=\"25%\">";
 strVar += "                                    <a href="+newBannerLink3+" style=\"display:block;border:none\" target=\"_blank\" data-saferedirecturl=\"\">";
 strVar += "                                    <img alt=\"xxx\" src="+newBannerImg3+ " \n \"";
 strVar += "			            style=\"background-color:#f6f2e9;border:none;color:#818181;display:";
@@ -201,102 +179,10 @@ strVar += "            <\/tr>";
 strVar += "        <\/table>";
 strVar += "    <\/div>";
 }
-}
-else if (ads_size=='ResponsiveSidebar')
-{
-	if (Rnd==0)
-	{
-	  var imgPrefix   = "imagesSidebar/";
-	  var randNum     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
-  
-	  var newBannerImg  = imgPrefix + adBanners[randNum];
-	  var newBannerLink = bannerLinks[randNum];
-
-	 // 728x214 728x90 banners 
-	strVar += "<link type=\"text/css\" rel=\"stylesheet\n \"";
-	strVar += "css\Ressidebar.css\">";
-	strVar += "<div id=\"w\">\n <div id=\"topad\">\n <a href="+newBannerLink+">";
-	strVar += "<img src="+newBannerImg+ " alt=\"\" class=\"Ressidebar\"/></a>\n </div> </div>";
-	}
-	else
-	{ // 300x300 banners
-
-  var imgPrefix   = "images300x300/";
-  var randNum     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
-  
-  var newBannerImg  = imgPrefix + adBanners[randNum];
-  var newBannerLink = bannerLinks[randNum];
-//
-  var imgPrefix2   = "images300x300/";
-  var randNum2     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
-  
-  var newBannerImg2  = imgPrefix2 + adBanners[randNum2];
-  var newBannerLink2 = bannerLinks[randNum2];
-
-  var imgPrefix3   = "images300x300/";
-  var randNum3     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
-  
-  var newBannerImg3  = imgPrefix3 + adBanners[randNum3];
-  var newBannerLink3 = bannerLinks[randNum3];
-
-  var imgPrefix4   = "images300x300/";
-  var randNum4     = Math.floor(Math.random() * ((adBanners.length-1) - 0 + 1)) + 0;
-  
-  var newBannerImg4  = imgPrefix4 + adBanners[randNum4];
-  var newBannerLink4 = bannerLinks[randNum4];
-
-
-strVar += "<div style=\"height: 250px;\" \"width: 250px;\">";
-strVar += "<link type=\"text/css\" rel=\"stylesheet\n \"";
-strVar += "href=\"css/Ressidebar.css\">";
-strVar += "        <table width=\"11%\" cellpadding=\"0\" border=\"1\" role=\"presentation\">";
-strVar += "            <tr>";
-strVar += "                <td>";
-strVar += "                    <table width=\"100%\" cellspacing=\"0\" cellpadding=\"1\" border=\"1\" align=\"center\">";
-strVar += "                        <tbody> ";
-strVar += "                            <tr>";
-strVar += "                                <td width=\"25%\">";
-strVar += "                                    <a href="+newBannerLink+" style=\"display:block;border:none\" target=\"_blank\" data-saferedirecturl=\"\">";
-strVar += "                                    <img alt=\"xxx\" src="+newBannerImg+ "\n \"";
-strVar += "			            style=\"background-color:#f6f2e9;border:none;color:#818181;display:";
-strVar += "			            block;font-size:9px;max-width:100%\" width=\"100%\"><\/a>";
-strVar += "                                <\/td>";
-strVar += "                            <\/tr>";
-strVar += "                            <tr>";
-strVar += "                                <td width=\"25%\">";
-strVar += "                                    <a href="+newBannerLink2+" style=\"display:block;border:none\" target=\"_blank\" data-saferedirecturl=\"\">";
-strVar += "                                    <img alt=\"xxx\" src="+newBannerImg2+ "\n \"";
-strVar += "			            style=\"background-color:#f6f2e9;border:none;color:#818181;display:";
-strVar += "			            block;font-size:9px;max-width:100%\" width=\"100%\"><\/a>";
-strVar += "                                <\/td>";
-strVar += "                            <\/tr>";
-strVar += "                            <tr>";
-strVar += "                                <td width=\"25%\">";
-strVar += "                                    <a href="+newBannerLink3+" style=\"display:block;border:none\" target=\"_blank\" data-saferedirecturl=\"\">";
-strVar += "                                    <img alt=\"xxx\" src="+newBannerImg3+ "\n \"";
-strVar += "			            style=\"background-color:#f6f2e9;border:none;color:#818181;display:";
-strVar += "			            block;font-size:9px;max-width:100%\" width=\"100%\"><\/a>";
-strVar += "                                <\/td>";
-strVar += "                            <\/tr>";
-strVar += "                            <tr>";
-strVar += "                                <td width=\"25%\">";
-strVar += "                                    <a href="+newBannerLink4+" style=\"display:block;border:none\" target=\"_blank\" data-saferedirecturl=\"\">";
-strVar += "                                    <img alt=\"xxx\" src="+newBannerImg4+ "\n \"";
-strVar += "			            style=\"background-color:#f6f2e9;border:none;color:#818181;display:";
-strVar += "			            block;font-size:9px;max-width:100%\" width=\"100%\"><\/a>";
-strVar += "                                <\/td>";
-strVar += "                            <\/tr>";
-strVar += "                        <\/tbody>";
-strVar += "                    <\/table>";
-strVar += "                <\/td>";
-strVar += "            <\/tr>";
-strVar += "        <\/table>";
-strVar += "    <\/div>";
-	}
-}
-
 document.getElementById("UAds").innerHTML = strVar;
 
 
 })
 // Main Ads End
+
+
