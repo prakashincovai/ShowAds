72cf63bf8f9df5880120d885cd2b5a017c199dbd
prakashincovai <prakashincovai@gmail.com>
2019-01-08 18:52:59 +0530

    Update ads-rotator.js

--- Basic Ads/JS_Controller/ads-rotator.js
+++ Basic Ads/JS_Controller/ads-rotator.js
@@ -268,7 +268,7 @@ else if (AdsRestriction2=='ResponsiveSidebar' && Js_two!=2)
         strVar += "<div style=\"height: 600px;\">";
         //strVar += "<link type=\"text/css\" rel=\"stylesheet\n \"";
         //strVar += "href=\"https://rawcdn.githack.com/prakashincovai/ShowAds/29c5ec7142781d91e0c975daea6a7d096c1e64f2/Basic%20Ads/css/Ressidebar.css\">";
-        strVar += "        <table width=\"100%\" cellpadding=\"0\" border=\"1\" role=\"presentation\">";
+        strVar += "        <table width=\"200px\" cellpadding=\"0\" border=\"1\" role=\"presentation\">";
         strVar += "            <tr>";
         strVar += "                <td>";
         strVar += "                    <table width=\"100%\" cellspacing=\"0\" cellpadding=\"1\" border=\"1\" align=\"center\">";
