0a13a6ce8573a29e295a0fc25340ddbd1598c31b
prakashincovai <prakashincovai@gmail.com>
2019-01-11 15:01:02 +0530

    Update 300x300_LinksGeneral.js

--- Basic Ads/JS_Links/300x300_LinksGeneral.js
+++ Basic Ads/JS_Links/300x300_LinksGeneral.js
@@ -1,6 +1,7 @@
 // Size 300x300 Responsive
 function BannerNames300x300Full() {
    adBanners = [
+       "s11012019_200x106.jpg",
        "run11012019_200_263.jpg",
        "Fire11012019_300x270.png",
        "cy10012019_300x136.jpg",
@@ -28,6 +29,7 @@ function BannerNames300x300Full() {
 
 function BannerLinks300x300Full() {
    bannerLinks = [
+       "https://clnk.in/hcDh",
        "https://clnk.in/hcCM",
        "https://clnk.in/hcCX",
        "https://clnk.in/hcip",
