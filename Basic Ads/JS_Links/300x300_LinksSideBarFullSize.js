de1c243f211c313a082807bd01bd9f5908b71df5
prakashincovai <prakashincovai@gmail.com>
2019-01-11 14:57:09 +0530

    Update 300x300_LinksSideBarFullSize.js

--- Basic Ads/JS_Links/300x300_LinksSideBarFullSize.js
+++ Basic Ads/JS_Links/300x300_LinksSideBarFullSize.js
@@ -1,6 +1,7 @@
 //Fullsidebar image 200x600 full banner needed
 function ResponsiveBannerNamesSideBarFull() {
    adBanners = [
+       "a11012019_300x600.jpg",
        "Gosidebar_160x600.gif",
        "Shoe08012019_200x600.jpeg"
   ];
@@ -8,6 +9,7 @@ function ResponsiveBannerNamesSideBarFull() {
 
 function ResponsiveBannerLinksSideBarFull() {
    bannerLinks = [
+       "https://clnk.in/hcAW",
        "https://clnk.in/gXeG",
        "http://fkrt.it/NFUETnuuuN"
   ];
